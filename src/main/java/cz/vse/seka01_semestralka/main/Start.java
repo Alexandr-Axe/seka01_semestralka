5824db1553d5fe7fffd304fb615d2d735f196ccc
Alexandr-Axe <sekera@arion.cz>
2023-03-09 21:09:33 +0100

    První commit

---
+++ src/main/java/cz/vse/seka01_semestralka/main/Start.java
@@ -0,0 +1,30 @@
+/* Soubor je ulozen v kodovani UTF-8.
+ * Kontrola kódování: Příliš žluťoučký kůň úpěl ďábelské ódy. */
+package cz.vse.seka01_semestralka.main;
+
+
+
+import cz.vse.seka01_semestralka.logika.*;
+import cz.vse.seka01_semestralka.uiText.TextoveRozhrani;
+
+/*******************************************************************************
+ * Třída  Start je hlavní třídou projektu,
+ * který představuje jednoduchou textovou adventuru určenou k dalším úpravám a rozšiřování
+ *
+ * @author    Jarmila Pavlíčková
+ * @version   ZS 2016/2017
+ */
+public class Start
+{
+    /***************************************************************************
+     * Metoda, prostřednictvím níž se spouští celá aplikace.
+     *
+     * @param args Parametry příkazového řádku
+     */
+    public static void main(String[] args)
+    {
+        IHra hra = new Hra();
+        TextoveRozhrani ui = new TextoveRozhrani(hra);
+        ui.hraj();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cz.vse.seka01_semestralka {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cz.vse.seka01_semestralka to javafx.fxml;
+    exports cz.vse.seka01_semestralka;
+}
